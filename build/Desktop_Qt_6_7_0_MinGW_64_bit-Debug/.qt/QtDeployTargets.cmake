92087e209689673ca5b83def5080f8a0708a4d8e
Yaroslav Makogon <yar.mcogon@gmail.com>
2025-07-06 00:34:18 +0300

    Delete build directory

--- build/Desktop_Qt_6_7_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_VisualTree_FILE C:/Users/YAROSLAV/Documents/VisualTree/build/Desktop_Qt_6_7_0_MinGW_64_bit-Debug/VisualTree.exe)
-set(__QT_DEPLOY_TARGET_VisualTree_RUNTIME_DLLS C:/Qt/6.7.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.0/mingw_64/bin/Qt6Core.dll)
